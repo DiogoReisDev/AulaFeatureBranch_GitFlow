f0e1c1aba991addaaea685835733d5a10471a323
Diogo Santana <diogo.anatnas@gmail.com>
2023-04-10 00:06:08 -0300

    Atualizando código para escolher produtos

--- feature_escolher_produtos.py
+++ feature_escolher_produtos.py
@@ -1,2 +1,11 @@
 def escolher_produtos():
     produtos = ['Hamburguer tradicional', 'Hamburguer vegano']
+    print(produtos)
+    select = input('Hamburguer tradicional digite 0: \nHamburguer Vegano digite 1: ')
+    if select == 0:
+        print('Você escolheu o Hamburguer tradicional!')
+    elif select == 1:
+        print('Você escolheu o Hamburguer Vegano!')
+    else:
+        print('Favor selecione as opções disponíveis...')
+escolher_produtos()
